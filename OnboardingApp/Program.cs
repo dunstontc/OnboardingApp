12f457ce6773eff53e43e25827823cf9753c1179
Clay Dunston <dunstontc@gmail.com>
2018-01-29 10:18:54 -0600

    Yet to implement setting Pin Num.

--- OnboardingApp/Program.cs
+++ OnboardingApp/Program.cs
@@ -2,55 +2,53 @@
 
 namespace OnboardingApp
 {
-    enum Choices_One
+    internal class Program
     {
-        Yes,
-        No
-    };
-    
-    enum Choices_Two
-    {
-        Yes,
-        No,
-        Maybe
-    };
-    
-    class Program
-    {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var theUser = new User();
-            theUser.First = AskQuestion("What is your first name?");
-            theUser.Last = AskQuestion("What is your last name?");
-            theUser.IsAccount = AskBool("Is this a personal account?");
-            Console.WriteLine(theUser.FullName());
+            theUser.First = PromptInput("What is your first name?");
+            theUser.Last = PromptInput("What is your last name?");
+            theUser.IsAccountOwner = AskBool("Are you the owner of this account?");
+            Console.Write(theUser.DescribeUser());
         }
 
-        private static string AskQuestion(string req)
+        private static string PromptInput(string prompt)
         {
-            Console.WriteLine(req);
+            Console.Clear();
+            Console.ResetColor();
+            
+            Console.WriteLine(prompt);
+            
             return Console.ReadLine();
         }
+
+//        public static string AskQuestion(string req)
+//        {
+//            var res = "";
+//
+//        }
+        
         
         public static bool AskBool(string prompt)
         {
             // Exit from program if CTL+C is pressed.
             Console.TreatControlCAsInput = false;
-            
+
             ConsoleKeyInfo cki;
-            
-            var res = "";
+
+            string res;
             var answer = false;
             var thePrompt = $"{prompt}\n(Y or N): ";
-            
+
             Console.Clear();
             Console.Write(thePrompt);
-            
-            do 
+
+            do
             {
                 cki = Console.ReadKey();
                 Console.ResetColor();
-                
+
                 switch (cki.KeyChar)
                 {
                     case 'y':
@@ -85,25 +83,7 @@ namespace OnboardingApp
             } while (cki.Key != ConsoleKey.Escape && cki.Key != ConsoleKey.Enter);
 
             Console.Clear();
-            Console.WriteLine($"You answered {res}.");
             return answer;
-
-        }
-
-        private static string AskEnum<T>(string prompt) where T : struct, IConvertible, IComparable, IFormattable
-        {
-            var type = typeof(T);
-            if (!type.IsEnum)
-            {
-                throw new Exception();
-            }
-
-            foreach (var test in Enum.GetValues(type))
-            {
-                Console.WriteLine("Testing " + test);
-            }
-
-            return "";
         }
     }
 }
--- OnboardingApp/User.cs
+++ OnboardingApp/User.cs
@@ -5,14 +5,58 @@ namespace OnboardingApp
     {
         public string First { get; set; }
         public string Last { get; set; }
-        
-        public bool IsAccount { get; set; }
-        
-        public int PinNumber { get; set; }
 
-        public string FullName()
+        private bool _isAccountOwner;
+        public bool IsAccountOwner
         {
-            return string.Join(" ", First, Last);
+            get { return _isAccountOwner; }
+            set { _isAccountOwner = value; }
+        }
+
+        private int _pinNumber;
+        public int PinNumber
+        {
+            get { return _pinNumber; }
+            set { _pinNumber = value; }
+        }
+
+
+        public string FullName
+        {
+            get
+            {
+                if (First.Length > 1 || Last.Length > 1)
+                {
+                    return string.Join(" ", First, Last);
+                }
+
+                return "This user has no name.";
+            }
+        }
+
+
+        public string IsOwner()
+        {
+            return IsAccountOwner ? "Account owner" : "Not account owner";
+        }
+
+        public string getPin()
+        {
+            if (PinNumber == 4)
+            {
+                return "****";
+            }
+
+            return "This user has yet to set up a PIN numner";
+        }
+
+        public string DescribeUser()
+        {
+            var userString = "User: " + FullName;
+            var ownerString = "Permissions: " + IsOwner();
+            var pinString = "PIN Number: " + getPin();
+            
+        return string.Join("\n", userString, ownerString, pinString);
         }
     }
 }
